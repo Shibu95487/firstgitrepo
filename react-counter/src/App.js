daf5df2ad0d2f84ba1355d478a84f63900b6f972
Shivang Yadav <shivakumar123@gmail.com>
2025-05-24 19:07:39 +0530

    update

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -1,34 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+
 function App() {
-  let count=0;
-  const [count,setcount]=usestate(0);
-
-function decresehandler(){
-    setCount=count-1;
-}
-
-function increasehandler(){
-  setCount=count+1;
-}
-function resethandler(){
-  setCount=0;
-}
-
   return (
-    <div className="w-[100vw] h-[100vw] flex justify-center item-center bg[#344151] flex-col gap-10">
-      <div>
-      Increment And Decrement
-      </div>
-      <div>
-        <button onclick={decresehandler}>+</button>
-        <button>{count}</button>
-        <button onclick={increaseehandler}>-</button>
-    </div>
-    <button onclick={resethandler}>
-      Reset
-    </button>
+    <div className="app-container">
+      <h1>
+        Hello, Shivang Yadav!
+        </h1>
+      <p>
+        Welcome to your React app 🎉
+        </p>
     </div>
   );
 }
